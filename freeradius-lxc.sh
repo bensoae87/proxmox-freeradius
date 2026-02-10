634ad55aa24efd50c5dff5e80997e770cc9e2873
Andrew Benson <bensoae87@Andrews-MacBook-Pro.local>
2026-02-10 12:49:12 -0600

    Latest Code

--- freeradius-lxc.sh
+++ freeradius-lxc.sh
@@ -1,54 +1,104 @@
 #!/usr/bin/env bash
-# Proxmox VE Helper Script: FreeRADIUS LXC
-
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 
-header_info
-check_root
-check_pve
-
-APP="FreeRADIUS"
-var_disk="8"
+# App metadata
+APP="FreeRADIUS + daloRADIUS"
+var_tags="radius,freeradius,daloradius,auth"
 var_cpu="2"
 var_ram="2048"
+var_disk="8"
 var_os="debian"
 var_version="12"
-var_unprivileged="1"
-var_features="keyctl=1,nesting=1"
-var_tags="radius;network"
+var_unprivileged="0"     # 0 = Privileged (YES)
+var_dhcp="1"             # 1 = DHCP (YES)
 var_hostname="freeradius"
 
-default_settings
+# UI
+header_info
+echo "This will create a FreeRADIUS + daloRADIUS LXC container."
+echo "Default Linux user: radusr / radusr"
+echo ""
 
-function install_freeradius() {
-  msg_info "Installing FreeRADIUS"
-  pct exec "$CTID" -- bash -c "
-    apt update &&
-    apt -y upgrade &&
-    apt -y install freeradius freeradius-utils
-  "
-  msg_ok "FreeRADIUS installed"
-}
-
-function configure_freeradius() {
-  msg_info "Configuring FreeRADIUS test user"
-  pct exec "$CTID" -- bash -c "
-cat <<EOF >> /etc/freeradius/4.0/mods-config/files/authorize
-radusr Cleartext-Password := \"radusr\"
-EOF
-systemctl enable freeradius
-systemctl restart freeradius
-  "
-  msg_ok "FreeRADIUS configured"
-}
-
+# Create container
+variables
+color
+catch_errors
 start
 build_container
-install_freeradius
-configure_freeradius
-finish
+
+# Container setup
+msg_info "Setting root password"
+pct exec $CTID -- bash -c "echo root:radusr | chpasswd"
+
+msg_info "Creating radusr user"
+pct exec $CTID -- bash -c "
+useradd -m -s /bin/bash radusr &&
+echo radusr:radusr | chpasswd &&
+usermod -aG sudo radusr
+"
+
+msg_info "Updating container"
+pct exec $CTID -- bash -c "apt update && apt -y upgrade"
+
+msg_info "Installing dependencies"
+pct exec $CTID -- bash -c "
+apt install -y \
+freeradius freeradius-mysql \
+mariadb-server \
+apache2 \
+php php-mysql php-gd php-xml php-mbstring php-curl php-zip \
+git unzip
+"
+
+msg_info "Configuring MariaDB"
+pct exec $CTID -- bash -c "
+systemctl enable --now mariadb &&
+mysql -e \"
+CREATE DATABASE radius;
+CREATE USER 'radius'@'localhost' IDENTIFIED BY 'radius';
+GRANT ALL PRIVILEGES ON radius.* TO 'radius'@'localhost';
+FLUSH PRIVILEGES;
+\"
+"
+
+msg_info "Importing FreeRADIUS schema"
+pct exec $CTID -- bash -c "
+mysql radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
+"
+
+msg_info "Installing daloRADIUS"
+pct exec $CTID -- bash -c "
+cd /var/www/html &&
+git clone https://github.com/lirantal/daloradius.git &&
+chown -R www-data:www-data daloradius &&
+chmod -R 755 daloradius
+"
+
+msg_info "Configuring daloRADIUS"
+pct exec $CTID -- bash -c "
+cp /var/www/html/daloradius/library/daloradius.conf.php.sample \
+   /var/www/html/daloradius/library/daloradius.conf.php &&
+sed -i \"s/'DB_PASSWORD' => '.*'/'DB_PASSWORD' => 'radius'/\" \
+   /var/www/html/daloradius/library/daloradius.conf.php
+"
+
+msg_info "Enabling services"
+pct exec $CTID -- bash -c "
+systemctl enable --now freeradius apache2
+"
 
 msg_ok "Installation complete!"
-echo
-echo "Test with:"
-echo "  pct exec $CTID -- radtest radusr radusr localhost 0 testing123"
+
+echo ""
+echo "Access daloRADIUS at:"
+echo "  http://<container-ip>/daloradius"
+echo ""
+echo "Default daloRADIUS credentials:"
+echo "  username: administrator"
+echo "  password: radius"
+echo ""
+echo "Linux user:"
+echo "  radusr / radusr"
+echo ""
+
+exit 0
